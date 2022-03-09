5864ce15b552d5f46984a8cc685fd1a32b0887d6
Andrzej Maćkowski <vandmac@icloud.com>
2022-03-09 08:49:38 +0100

    repair hide hamburger button after click

--- js/main.js
+++ js/main.js
@@ -3,13 +3,18 @@ const hamburgerMenuCloseBtn = document.getElementById(
   'hamburger-menu-close-btn'
 );
 const mainContainer = document.querySelector('.main-container');
+const mobileMenuSection = document.querySelector('.mobile-menu-section');
 
-hamburgerMenuBtn.addEventListener('click', () =>
-  mainContainer.classList.add('hamburger-menu-activated')
-);
-hamburgerMenuCloseBtn.addEventListener('click', () =>
-  mainContainer.classList.remove('hamburger-menu-activated')
-);
+hamburgerMenuBtn.addEventListener('click', () => {
+  mainContainer.classList.add('hamburger-menu-activated');
+  hamburgerMenuBtn.style.display = 'none';
+  mobileMenuSection.style.display = 'flex';
+});
+hamburgerMenuCloseBtn.addEventListener('click', () => {
+  mainContainer.classList.remove('hamburger-menu-activated');
+  hamburgerMenuBtn.style.display = 'block';
+  mobileMenuSection.style.display = 'none';
+});
 
 const myAudioPlayer = document.getElementById('my-audio-background');
 myAudioPlayer.volume = 0.02;
