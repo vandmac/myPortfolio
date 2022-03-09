90842dc1eca47522d18384286a6bc0734174b47a
Andrzej Maćkowski <vandmac@icloud.com>
2022-03-09 16:34:20 +0100

    add icons menu second commit

--- js/main.js
+++ js/main.js
@@ -19,4 +19,16 @@ hamburgerMenuCloseBtn.addEventListener('click', () => {
 const myAudioPlayer = document.getElementById('my-audio-background');
 myAudioPlayer.volume = 0.02;
 
+// popover initialize
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
 
+// enable tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
